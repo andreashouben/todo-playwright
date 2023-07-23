72fee97bd3a835d7bc07db6c9375efdb7af6b1e9
Andreas Houben <andreas.houben@codecentric.de>
2023-07-23 21:39:28 +0200

    finish service

--- src/app/todo.service.spec.ts
+++ src/app/todo.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import {AddTodo, TodoService} from './todo.service';
+import { AddTodo, TodoService } from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
@@ -11,54 +11,93 @@ describe('TodoService', () => {
   });
 
   it('returns an empty list of todos initially', () => {
-    expect(service.getTodos()).toEqual([])
+    expect(service.getTodos()).toEqual([]);
   });
 
-  describe('after adding one todo', () =>{
-    const todoToAdd:AddTodo = {text: 'Clean Garage'}
-    beforeEach(() =>{
-      service.addTodo(todoToAdd)
-    })
+  describe('after adding one todo', () => {
+    const todoToAdd: AddTodo = { text: 'Clean Garage' };
+    beforeEach(() => {
+      service.addTodo(todoToAdd);
+    });
 
-    it('returns a list containing the todo', ()=>{
+    it('returns a list containing the todo', () => {
       const expectedTodo: Todo = {
         id: 0,
         text: todoToAdd.text,
-        done: false
-      }
+        done: false,
+      };
 
-      const [actualTodo] =service.getTodos()
+      const [actualTodo] = service.getTodos();
 
-      expect(actualTodo).toEqual(expectedTodo)
-    })
+      expect(actualTodo).toEqual(expectedTodo);
+    });
 
-    it('can mark a todo as done', () =>{
+    it('can mark a todo as done', () => {
       const expectedTodo: Todo = {
         id: 0,
         text: todoToAdd.text,
-        done: true
-      }
-      const [addedTodo] =service.getTodos()
+        done: true,
+      };
+      const [addedTodo] = service.getTodos();
 
-      service.markAsDone(addedTodo.id)
+      service.markAsDone(addedTodo.id);
       const [doneTodo] = service.getTodos();
 
-      expect(doneTodo).toEqual(expectedTodo)
-    })
+      expect(doneTodo).toEqual(expectedTodo);
+    });
 
-    it('can mark a done todo as undone', () =>{
+    it('can mark a done todo as undone', () => {
       const expectedTodo: Todo = {
         id: 0,
         text: todoToAdd.text,
-        done: false
-      }
-      const [addedTodo] =service.getTodos()
-      service.markAsDone(addedTodo.id)
+        done: false,
+      };
+      const [addedTodo] = service.getTodos();
+      service.markAsDone(addedTodo.id);
 
+      service.markAsUndone(addedTodo.id);
+      const [unDoneTodo] = service.getTodos();
 
+      expect(unDoneTodo).toEqual(expectedTodo);
+    });
 
-    })
+    it('can archive a todo', () => {
+      const [addedTodo] = service.getTodos();
 
+      service.archiveTodo(addedTodo.id);
 
-  })
+      const todos = service.getTodos();
+
+      expect(todos).toEqual([]);
+    });
+
+    describe('with an archived todo', () => {
+      beforeEach(() => {
+        const [addedTodo] = service.getTodos();
+
+        service.archiveTodo(addedTodo.id);
+      });
+
+      it('lists archived todos', () => {
+        const expectedTodo: Todo = {
+          id: 0,
+          text: todoToAdd.text,
+          done: false,
+        };
+
+        const archivedTodos = service.getArchivedTodos();
+
+        expect(archivedTodos).toEqual([expectedTodo]);
+      });
+
+      it('unarchives todo', () => {
+        const [archivedTodo] = service.getArchivedTodos();
+
+        service.unarchiveTodo(archivedTodo.id);
+
+        expect(service.getArchivedTodos()).toEqual([]);
+        expect(service.getTodos()).toEqual([archivedTodo]);
+      });
+    });
+  });
 });
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,32 +1,56 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { partition } from 'ramda';
 
-export type AddTodo = { text: string }
+export type AddTodo = { text: string };
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  private todos: Todo[] = [];
+  private archive: Todo[] = [];
+  private nextIndex = 0;
 
-    private todos: Todo[] = []
-    private nextIndex = 0;
+  constructor() {}
 
-    constructor() {
-    }
+  getTodos() {
+    return Object.seal([...this.todos]);
+  }
 
-    getTodos() {
-        return Object.seal([...this.todos]);
-    }
+  addTodo(addTodo: AddTodo) {
+    const newTodo: Todo = {
+      id: this.nextIndex++,
+      text: addTodo.text,
+      done: false,
+    };
+    this.todos = [...this.todos, newTodo];
+  }
 
-    addTodo(addTodo: AddTodo) {
-        const newTodo: Todo = {
-            id: this.nextIndex++,
-            text: addTodo.text,
-            done: false
-        }
-        this.todos = [...this.todos, newTodo]
-    }
+  markAsDone(id: number) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id ? { ...todo, done: true } : todo,
+    );
+  }
 
-    markAsDone(id: number) {
-        this.todos = this.todos.map(todo => todo.id === id ? {...todo, done: true} : todo)
-    }
+  markAsUndone(id: number) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id ? { ...todo, done: false } : todo,
+    );
+  }
+
+  archiveTodo(id: number) {
+    const [archive, todos] = partition((todo) => todo.id === id, this.todos);
+    this.todos = todos;
+    this.archive = archive;
+  }
+
+  unarchiveTodo(id: number) {
+    const [todos, archive] = partition((todo) => todo.id === id, this.archive);
+    this.todos = todos;
+    this.archive = archive;
+  }
+
+  getArchivedTodos() {
+    return Object.seal([...this.archive]);
+  }
 }
