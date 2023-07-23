590bd58041c744121deecfdfe66807895c53253c
Andreas Houben <andreas.houben@codecentric.de>
2023-07-23 21:53:34 +0200

    implement form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {TodoListComponent} from "./todo-list.component";
+import { TodoListComponent } from './todo-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TodoListComponent],
+  imports: [BrowserModule, TodoListComponent, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/todo-list.component.ts
+++ src/app/todo-list.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoService } from './todo.service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'todo-todo-list',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <p>todo-list works!</p> `,
+  imports: [CommonModule, ReactiveFormsModule],
+  template: ` <form [formGroup]="form" (ngSubmit)="onSubmit()">
+      <label>Enter a todo:<input type="text" formControlName="todo" /></label>
+    </form>
+    {{ form.getRawValue() | json }}
+    <ul>
+      <li *ngFor="let todoItem of todos">
+        {{ todoItem.text }}
+      </li>
+    </ul>`,
   styles: [``],
 })
-export class TodoListComponent {}
+export class TodoListComponent implements OnInit {
+  form = this.fb.group({
+    todo: ['', Validators.required],
+  });
+
+  todos: Todo[] = [];
+  constructor(
+    private todoService: TodoService,
+    private fb: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.todos = this.todoService.getTodos();
+  }
+
+  onSubmit() {
+    const text = this.form.get('todo')?.value!;
+    this.todoService.addTodo({ text });
+    this.form.reset();
+    this.todos = this.todoService.getTodos();
+  }
+}
