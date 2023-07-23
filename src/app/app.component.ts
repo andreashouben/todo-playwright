2d206b494758c60d103242835e7ea49e3e5ff433
Andreas Houben <andreas.houben@codecentric.de>
2023-07-23 17:36:43 +0200

    switch prefix to todo and remove template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'todo-root',
+  template: `<div>App Works</div>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
